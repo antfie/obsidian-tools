d12fc80debce74180fb04d102dca123dc745e2d5
Anthony Fielding <anthony@stickerdocs.com>
2024-10-17 20:57:11 +0100

    Refactor

--- find_duplicates.go
+++ find_duplicates.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/sha512"
-	"encoding/hex"
 	"log"
 	"os"
 	"path"
@@ -51,14 +49,4 @@ func findDuplicates(source string) {
 			}
 		}
 	}
-}
-
-func Sha512(data []byte) []byte {
-	hash := sha512.New512_256()
-	hash.Write(data)
-	return hash.Sum(nil)
-}
-
-func Sha512ToString(data []byte) string {
-	return hex.EncodeToString(Sha512(data))
 }
